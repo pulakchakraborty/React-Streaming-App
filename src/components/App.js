6f42956b0a44995ab46a5ca5f6f8dc12860bca20
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-04-23 13:38:51 +0200

    Basic routing with react router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const home = () => {
+    return(
+        <div>Home Page</div>
+    );
+};
+
+const about = () => {
+    return(
+        <div>About Page</div>
+    );
+};
 
 const App = () => {
     return(
-        <div>From App component</div>
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={home}>Home Page</Route>
+                    <Route path="/about" component={about}>About Page</Route>
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
